cd20f913b6cea6ef1fef11e7e33aba252f406526
rockstaryao <670139644@qq.com>
2020-10-21 09:56:18 +0800

    测试api接口

--- dx-boot-serve/src/main/java/com/dx/open/controller/DemoController.java
+++ dx-boot-serve/src/main/java/com/dx/open/controller/DemoController.java
@@ -1,6 +1,8 @@
 package com.dx.open.controller;
 
 import com.dx.common.util.AjaxResult;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,12 +15,21 @@ import org.springframework.web.bind.annotation.RestController;
  * @copyright Copyright (c) 文理电信
  * @since 2020/9/17
  */
+@Api(tags = "测试类")
 @RestController
 @Slf4j
 public class DemoController {
 
+    @ApiOperation(value = "前后端接口", notes = "查看前后端是否通")
     @PostMapping("/openApi/getList")
     public Object getList(){
+        log.info("请求后台数据！");
+        return AjaxResult.success("请求成功！");
+    }
+
+    @ApiOperation(value = "测试接口1", notes = "测试接口1")
+    @PostMapping("/openApi/insetData")
+    public Object insetData(){
         log.info("请求后台数据！");
         return AjaxResult.success("请求成功！");
     }
