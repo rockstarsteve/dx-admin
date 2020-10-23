d3dada44fddc79005a76d6f94617fe622c08033e
rockstaryao <670139644@qq.com>
2020-10-23 09:04:55 +0800

    解决问题

--- dx-boot-serve/src/main/java/com/dx/open/controller/DemoController.java
+++ dx-boot-serve/src/main/java/com/dx/open/controller/DemoController.java
@@ -1,9 +1,11 @@
 package com.dx.open.controller;
 
 import com.dx.common.util.AjaxResult;
+import com.dx.open.service.IBcdCardService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,6 +22,9 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 public class DemoController {
 
+    @Autowired
+    private IBcdCardService iBcdCardService;
+
     @ApiOperation(value = "前后端接口", notes = "查看前后端是否通")
     @PostMapping("/openApi/getList")
     public Object getList(){
@@ -27,11 +32,13 @@ public class DemoController {
         return AjaxResult.success("请求成功！");
     }
 
-    @ApiOperation(value = "测试接口1", notes = "测试接口1")
+    @ApiOperation(value = "批量保存", notes = "测试接口1")
     @PostMapping("/openApi/insetData")
     public Object insetData(){
         log.info("请求后台数据！");
-        return AjaxResult.success("请求成功！");
+
+
+
     }
 
 }
